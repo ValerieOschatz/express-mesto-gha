ed8751bfd6e0430699802496f90275c9643f8faf
Valerie K <valerik210695@gmail.com>
2022-08-16 22:06:45 +0300

    add user errors

--- controllers/cards.js
+++ controllers/cards.js
@@ -22,13 +22,13 @@ module.exports.deleteCard = (req, res) => {
 };
 
 module.exports.addLike = (req, res) => {
-  Card.findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: req.user._id } })
+  Card.findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: req.user._id } }, { new: true })
     .then((card) => res.send({ data: card }))
     .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
 };
 
 module.exports.removeLike = (req, res) => {
-  Card.findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } })
+  Card.findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } }, { new: true })
     .then((card) => res.send({ data: card }))
     .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
 };
--- controllers/users.js
+++ controllers/users.js
@@ -1,21 +1,28 @@
 const User = require('../models/user');
+const { BAD_REQUEST, NOT_FOUND, INTERNAL_SERVER_ERROR } = require('../utils/errorCodes');
 
 module.exports.getUsers = (req, res) => {
   User.find({})
     .then((users) => res.send({ data: users }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch(() => res.status(INTERNAL_SERVER_ERROR).send({ message: 'Произошла ошибка' }));
 };
 
 module.exports.getUser = (req, res) => {
   User.findById(req.params.userId)
     .then((user) => {
-      if (!user) {
-        res.send({ error: 'Такого пользователя нет' });
-        return;
+      if (user) {
+        res.send({ data: user });
+      } else {
+        res.status(NOT_FOUND).send({ message: 'Запрашиваемый пользователь не найден' });
       }
-      res.send({ data: user });
     })
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(BAD_REQUEST).send({ message: 'Переданы некорректные данные' });
+      } else {
+        res.status(INTERNAL_SERVER_ERROR).send({ message: 'Произошла ошибка' });
+      }
+    });
 };
 
 module.exports.createUser = (req, res) => {
@@ -23,7 +30,13 @@ module.exports.createUser = (req, res) => {
 
   User.create({ name, about, avatar })
     .then((user) => res.send({ data: user }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(BAD_REQUEST).send({ message: 'Переданы некорректные данные' });
+      } else {
+        res.status(INTERNAL_SERVER_ERROR).send({ message: 'Произошла ошибка' });
+      }
+    });
 };
 
 module.exports.updateUser = (req, res) => {
@@ -31,13 +44,13 @@ module.exports.updateUser = (req, res) => {
 
   User.findByIdAndUpdate(req.user._id, { name, about })
     .then((user) => {
-      if (!user) {
-        res.send({ error: 'Такого пользователя нет' });
-        return;
+      if (name && about) {
+        res.send({ data: user });
+      } else {
+        res.status(BAD_REQUEST).send({ message: 'Переданы некорректные данные' });
       }
-      res.send({ data: user });
     })
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch(() => res.status(INTERNAL_SERVER_ERROR).send({ message: 'Произошла ошибка' }));
 };
 
 module.exports.updateAvatar = (req, res) => {
@@ -45,11 +58,11 @@ module.exports.updateAvatar = (req, res) => {
 
   User.findByIdAndUpdate(req.user._id, { avatar })
     .then((user) => {
-      if (!user) {
-        res.send({ error: 'Такого пользователя нет' });
-        return;
+      if (avatar) {
+        res.send({ data: user });
+      } else {
+        res.status(BAD_REQUEST).send({ message: 'Переданы некорректные данные' });
       }
-      res.send({ data: user });
     })
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch(() => res.status(INTERNAL_SERVER_ERROR).send({ message: 'Произошла ошибка' }));
 };
