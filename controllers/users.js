836c5b77716fe7a7caa66c7b6afc23e42201c711
Valerie K <valerik210695@gmail.com>
2022-08-29 13:38:25 +0300

    add email and password

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,6 @@
+const bcrypt = require('bcryptjs');
 const User = require('../models/user');
+
 const {
   BAD_REQUEST,
   NOT_FOUND,
@@ -33,12 +35,28 @@ const getUser = async (req, res) => {
 
 const createUser = async (req, res) => {
   try {
-    const { name, about, avatar } = req.body;
-    const user = await User.create({ name, about, avatar });
+    const {
+      name,
+      about,
+      avatar,
+      email,
+      password,
+    } = req.body;
+
+    const hash = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      about,
+      avatar,
+      email,
+      password: hash,
+    });
+
     return res.status(CREATED).send({ user });
   } catch (err) {
     if (err.name === 'ValidationError') {
-      return res.status(BAD_REQUEST).send({ message: `Переданы некорректные данные${err.errors.name ? err.errors.name : ''}${err.errors.about ? err.errors.about : ''}${err.errors.avatar ? err.errors.avatar : ''}` });
+      return res.status(BAD_REQUEST).send({ message: `Переданы некорректные данные${err.errors.name ? err.errors.name : ''}${err.errors.about ? err.errors.about : ''}${err.errors.avatar ? err.errors.avatar : ''}${err.errors.email ? err.errors.email : ''}` });
     }
     return res.status(INTERNAL_SERVER_ERROR).send({ message: 'Произошла ошибка' });
   }
--- models/user.js
+++ models/user.js
@@ -1,21 +1,35 @@
 const mongoose = require('mongoose');
+const { isEmail } = require('validator');
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, ', поле name обязательное'],
     minlength: [2, ', поле name должно содержать не менее 2 символов'],
     maxlength: [30, ', поле name должно содержать не более 30 символов'],
+    default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
-    required: [true, ', поле about обязательное'],
     minlength: [2, ', поле about должно содержать не менее 2 символов'],
-    maxlength: [30, ', поле name должно содержать не более 30 символов'],
+    maxlength: [30, ', поле about должно содержать не более 30 символов'],
+    default: 'Исследователь',
   },
   avatar: {
     type: String,
-    required: [true, ', поле avatar обязательное'],
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+  },
+  email: {
+    type: String,
+    required: [true, ', поле email обязательное'],
+    unique: [true, ', пользователь с этим email уже существует'],
+    validate: {
+      validator: (v) => isEmail(v),
+      message: ', введите корректный email',
+    },
+  },
+  password: {
+    type: String,
+    required: [true, ', поле password обязательное'],
   },
 });
 
